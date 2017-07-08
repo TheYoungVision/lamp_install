299cf2cd45a4e96079d129a412688dc91f30f18e
TheYoungVision <TheYoungVision@protonmail.com>
2017-07-08 19:17:42 +0200

    Fix type

--- lamp_install.sh
+++ lamp_install.sh
@@ -7,6 +7,6 @@ apt install curl apache2 php php-cli libapache2-mod-php composer mysql-server my
 curl -sS https://getcomposer.org/installer | php -- --install-dir=/usr/local/bin --filename=composer
 rm -f /var/www/html/index.html
 touch /var/www/html/index.php
-sed -i '1a <?php\n\tphpinfo();/n?>'
+sed -i '1a <?php\n\tphpinfo();\n?>'
 
 service apache2 restart
