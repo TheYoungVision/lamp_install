65bdd92e0fef233e341a9948da6f474af78c8b75
user <user@debian>
2017-07-08 19:13:22 +0200

    First commit

---
+++ lamp_install.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+# Launch it as root
+
+apt update && apt upgrade
+apt install curl apache2 php php-cli libapache2-mod-php composer mysql-server mysql-client php-mysql phpmyadmin
+curl -sS https://getcomposer.org/installer | php -- --install-dir=/usr/local/bin --filename=composer
+rm -f /var/www/html/index.html
+touch /var/www/html/index.php
+sed -i '1a <?php\n\tphpinfo();/n?>'
+
+service apache2 restart
